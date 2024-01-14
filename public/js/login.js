e009a3a930bb0148b28c344c4590498299622f7d
arkenwald <149994852+arkenwald@users.noreply.github.com>
2024-01-13 19:34:12 -0500

    small update to classes in login.js to reflect login.handlebars

--- public/js/login.js
+++ public/js/login.js
@@ -2,10 +2,10 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const name = document.querySelector('#loginUsername').value.trim();
+  const password = document.querySelector('#loginPassword').value.trim();
 
-  if (email && password) {
+  if (name && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
@@ -25,9 +25,9 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const name = document.querySelector('#createUsername').value.trim();
+  const email = document.querySelector('#createEmail').value.trim();
+  const password = document.querySelector('#createPassword').value.trim();
 
   if (name && email && password) {
     const response = await fetch('/api/users', {
