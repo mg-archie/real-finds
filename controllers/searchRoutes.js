ff93afaeac62c9261b0282dc09b7841389d5456b
Faust Celaj <faustcelaj@macbook-pro.home>
2024-01-14 22:09:28 -0500

    added styling

--- controllers/searchRoutes.js
+++ controllers/searchRoutes.js
@@ -1,27 +1,35 @@
 const router = require('express').Router();
-const { User, Agent, Listing, } = require('../models');
+const { User, Agent, Listing } = require('../models');
 
 // Search route
 router.get('/', async (req, res) => {
-    try {
-      const { query } = req.query;
-  
-      // Perform the search logic based on the query parameter
-      const searchResults = await performSearch(query);
-  
-      // Render the search page with the search results
-      res.render('search', { searchResults, logged_in: req.session.logged_in });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json(err);
+  try {
+    const { query } = req.query;
+
+    let searchResults;
+
+    if (query) {
+      // If the query is not empty, perform the search logic based on the query parameter
+      searchResults = await performSearch(query);
+    } else {
+      // If the query is empty, retrieve all listings (modify this logic based on your database schema)
+      searchResults = await Listing.findAll();
     }
-  });
-  
-  // Function to perform the search logic
-  async function performSearch(query) {
-    // search logic here based on the query parameter
-    // ex, search for listings or agents that match the query
-    // Return the search results
+
+    // Render the search page with the search results
+    res.render('search', { searchResults, });
+  } catch (err) {
+    console.log(err)
+    console.error(err);
+    res.status(500).json(err);
   }
-  
-  module.exports = router;
+});
+
+// Function to perform the search logic
+async function performSearch(query) {
+  // Perform your search logic here based on the query parameter
+  // For example, you can search for listings or agents that match the query
+  // Return the search results
+}
+
+module.exports = router;
